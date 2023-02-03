13369cfabdbc4fb84259a31f055fb4964d6fc17d
gorbatschow <gorbatschow.r@gmail.com>
2023-02-04 01:47:52 +0300

    add exception if method is not implemented

--- IPixelGrid.h
+++ IPixelGrid.h
@@ -1,9 +1,16 @@
 #pragma once
 #include "imgui.h"
 #include <imgui.h>
+#include <stdexcept>
 #include <vector>
 
 class IPixelGrid {
+  class NotImplementedException : public std::logic_error {
+  public:
+    NotImplementedException(const std::string &func_name)
+        : std::logic_error{"IPixelGrid::" + func_name + " not implemented"} {}
+  };
+
 public:
   virtual ~IPixelGrid() = default;
 
@@ -12,6 +19,7 @@ public:
   // to Pixel
   virtual const std::vector<std::size_t> &
   indexToPixel(std::size_t index) const {
+    throw NotImplementedException(__func__);
     return _emptyPixel;
   };
 
@@ -20,25 +28,36 @@ public:
   // to Pixel
   virtual const std::vector<std::size_t> &nodeToPixel(std::size_t dim_1,
                                                       std::size_t dim_2) const {
+    throw NotImplementedException(__func__);
     return _emptyPixel;
   };
 
   // Grid Size
-  virtual std::size_t gridSize(std::size_t dim = 0) const { return 0; }
+  virtual std::size_t gridSize(std::size_t dim = 0) const {
+    throw NotImplementedException(__func__);
+    return 0;
+  }
 
   // Pixel
   //----------------------------------------------------------------------------
   // Pixel Width
-  virtual std::size_t pixelWidth() const { return 0; }
+  virtual std::size_t pixelWidth() const {
+    throw NotImplementedException(__func__);
+    return 0;
+  }
 
   // Pixel Height
-  virtual std::size_t pixelHeight() const { return 0; }
+  virtual std::size_t pixelHeight() const {
+    throw NotImplementedException(__func__);
+    return 0;
+  }
 
   // Polar
   //----------------------------------------------------------------------------
   // to Pixel
   virtual const std::vector<std::size_t> &polarToPixel(double r,
                                                        double phi) const {
+    throw NotImplementedException(__func__);
     return _emptyPixel;
   };
 
@@ -46,77 +65,111 @@ public:
   virtual bool
   sectorToPixel(double r, double phi_min, double phi_max,
                 std::vector<std::vector<std::size_t>> &pixel_list) const {
+    throw NotImplementedException(__func__);
     return false;
   }
 
   // to Index
-  virtual void polarToIndex(double r, double phi, std::size_t &index) const {}
+  virtual void polarToIndex(double r, double phi, std::size_t &index) const {
+    throw NotImplementedException(__func__);
+  }
 
   // to Node
   virtual void polarToNode(double r, double phi, std::size_t &dim_1,
-                           std::size_t &dim_2) const {}
+                           std::size_t &dim_2) const {
+    throw NotImplementedException(__func__);
+  }
 
   // Distance Nodes
   virtual const std::vector<double> &distanceNodes(double bearing = 0) const {
+    throw NotImplementedException(__func__);
     return _emptyNodes;
   }
 
   // Bearing Nodes
   virtual const std::vector<double> &bearingNodes(double distance = 0) const {
+    throw NotImplementedException(__func__);
     return _emptyNodes;
   }
 
   // Distance Bounds
   virtual bool distanceBounds(double r, double &min, double &max) const {
+    throw NotImplementedException(__func__);
     return false;
   }
 
   // Bearing Bounds
   virtual bool bearingBounds(double phi, double &min, double &max) const {
+    throw NotImplementedException(__func__);
     return false;
   }
 
   // Polar Contains
-  virtual bool polarContains(double r, double phi) const { return false; }
+  virtual bool polarContains(double r, double phi) const {
+    throw NotImplementedException(__func__);
+    return false;
+  }
 
   // Make Polar Mesh
   virtual void makePolarMesh(std::vector<double> &r,
-                             std::vector<double> &phi) const {}
+                             std::vector<double> &phi) const {
+    throw NotImplementedException(__func__);
+  }
 
   // Cartesian
   //----------------------------------------------------------------------------
   // to Pixel
   virtual const std::vector<std::size_t> &cartesianToPixel(double x,
                                                            double y) const {
+    throw NotImplementedException(__func__);
     return _emptyPixel;
   };
 
   // to Index
-  virtual void cartesianToIndex(double x, double y, std::size_t &index) const {}
+  virtual void cartesianToIndex(double x, double y, std::size_t &index) const {
+    throw NotImplementedException(__func__);
+  }
 
   // to Node
   virtual void cartesianToNode(double x, double y, std::size_t &dim_1,
-                               std::size_t &dim_2) const {}
+                               std::size_t &dim_2) const {
+    throw NotImplementedException(__func__);
+  }
 
   // Cartesian Bounds Min
-  virtual ImVec2 cartesianBoundsMin() const { return {0, 0}; };
+  virtual ImVec2 cartesianBoundsMin() const {
+    throw NotImplementedException(__func__);
+    return {0, 0};
+  };
 
   // Cartesian Bounds Max
-  virtual ImVec2 cartesianBoundsMax() const { return {0, 0}; };
+  virtual ImVec2 cartesianBoundsMax() const {
+    throw NotImplementedException(__func__);
+    return {0, 0};
+  };
 
   // Make Cartesian Mesh
   virtual void makeCartesianMesh(std::vector<double> &x,
-                                 std::vector<double> &y) const {}
+                                 std::vector<double> &y) const {
+    throw NotImplementedException(__func__);
+  }
 
   // Cartesian Contains
-  virtual bool cartesianContains(double x, double y) const { return false; }
+  virtual bool cartesianContains(double x, double y) const {
+    throw NotImplementedException(__func__);
+    return false;
+  }
 
 protected:
   inline const std::vector<std::size_t> &emptyPixel() const {
+    throw NotImplementedException(__func__);
     return _emptyPixel;
   }
 
-  inline const std::vector<double> &emptyNodes() const { return _emptyNodes; }
+  inline const std::vector<double> &emptyNodes() const {
+    throw NotImplementedException(__func__);
+    return _emptyNodes;
+  }
 
 private:
   const std::vector<std::size_t> _emptyPixel{};
